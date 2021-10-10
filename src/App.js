a6caccfd4d21fdb0a1a53657ba4c0ad110fb012f
FoxCrush <63466872+FoxCrush@users.noreply.github.com>
2021-10-10 12:52:56 +0300

    step 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
+  createContact = (name) => {
+    const contact = {
+      name,
+      id: uuidv4(),
+    };
+    this.setState((currState) => ({
+      contacts: [...currState.contacts, contact],
+    }));
+  };
+
+  addContactButtonHandler = (e) => {
+    e.preventDefault();
+    if (this.state.name.length > 0) {
+      this.createContact(this.state.name);
+    }
+
+    this.setState({ name: "" });
+  };
+  onInputChangeHandler = (e) => {
+    e.preventDefault();
+    this.setState({ name: e.target.value });
+  };
+  deleteCobtact = () => {
+    this.setState((currState) => ({}));
+  };
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <section className="phonebookSection">
+        <h1>Phonebook</h1>
+        <div className="createContactContainer">
+          <h3>Name</h3>
+          <input
+            value={this.state.name}
+            onChange={this.onInputChangeHandler}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+          />
+          <button
+            className="addContactButton"
+            type="button"
+            onClick={this.addContactButtonHandler}
+          >
+            Add contact
+          </button>
+        </div>
+        <h2>Contacts</h2>
+        <div className="contactListContainer">
+          <ul>
+            {contacts.map(({ id, name }) => (
+              <li key={id}>{name}</li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
